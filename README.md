b017981470fdc2372604fe368242b95d10f73064
hkevin555 <157328999+hkevin555@users.noreply.github.com>
2024-02-14 23:13:27 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Delta
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hkevin555/pen/dyrwqNw](https://codepen.io/hkevin555/pen/dyrwqNw).
+
---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function generateSpiritAnimal() {
+  var name = document.getElementById("name").value;
+  var reversedName = name.split('').reverse().join('');
+  var spiritAnimals = ['Wolf', 'Eagle', 'Dolphin', 'Bear', 'Fox', 'Tiger', 'Lion', 'Hawk', 'Owl', 'Panther'];
+  var randomSpiritAnimal = spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
+  var output = document.getElementById("output");
+  output.innerHTML = "<p>Your spirit animal is: " + randomSpiritAnimal + "</p>";
+  output.innerHTML += "<p>Your left handed name: " + reversedName + "</p>";
+}
