56fa0a4dfe269784126adf189c1bd4d4953fe9e7
hkevin555 <157328999+hkevin555@users.noreply.github.com>
2024-02-22 12:51:52 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
+  $('#generateButton').click(function() {
+    generateSpiritAnimal();
+  });
+
   function generateSpiritAnimal() {
-  var name = document.getElementById("name").value;
-  var reversedName = name.split('').reverse().join('');
-  var spiritAnimals = ['Wolf', 'Eagle', 'Dolphin', 'Bear', 'Fox', 'Tiger', 'Lion', 'Hawk', 'Owl', 'Panther'];
-  var randomSpiritAnimal = spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
-  var output = document.getElementById("output");
-  output.innerHTML = "<p>Your spirit animal is: " + randomSpiritAnimal + "</p>";
-  output.innerHTML += "<p>Your left handed name: " + reversedName + "</p>";
-}
+    var name = $('#name').val();
+    var reversedName = name.split('').reverse().join('');
+    var spiritAnimals = ['Wolf', 'Eagle', 'Dolphin', 'Bear', 'Fox', 'Tiger', 'Lion', 'Hawk', 'Owl', 'Panther'];
+    var randomSpiritAnimal = spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
+    var output = $('#output');
+    output.html("<p>Your spirit animal is: " + randomSpiritAnimal + "</p>");
+    output.append("<p>Your left handed name: " + reversedName + "</p>");
+  }
 });
